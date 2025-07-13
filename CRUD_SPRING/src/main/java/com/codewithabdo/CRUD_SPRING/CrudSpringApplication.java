d8d1a8650f8e5770abde97d1659e5288951a10d8
hamedragaab22 <hamedragab538@gmail.com>
2025-07-13 17:35:39 +0300

    Created spring boot structure

---
+++ CRUD_SPRING/src/main/java/com/codewithabdo/CRUD_SPRING/CrudSpringApplication.java
@@ -0,0 +1,13 @@
+package com.codewithabdo.CRUD_SPRING;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+}
